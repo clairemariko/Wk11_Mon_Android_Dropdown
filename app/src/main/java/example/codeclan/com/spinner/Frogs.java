123b2cf4c28eadb4cb4b30de19c5d3f1c6049291
claire <claire.mariko.smith@gmail.com>
2016-06-16 16:40:10 +0100

    update

--- app/src/main/java/example/codeclan/com/spinner/Frogs.java
+++ app/src/main/java/example/codeclan/com/spinner/Frogs.java
@@ -2,6 +2,7 @@ package example.codeclan.com.spinner;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -31,7 +32,7 @@ public class Frogs extends AppCompatActivity {
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         // On selecting a spinner item
         String item = parent.getItemAtPosition(position).toString();
-
+        Log.d("froglog", item);
         // Showing selected spinner item
         Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
     }
@@ -40,5 +41,5 @@ public class Frogs extends AppCompatActivity {
 
     }
 
-   
+
 }
